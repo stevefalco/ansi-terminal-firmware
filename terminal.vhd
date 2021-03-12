ef76d8ae93ed4f7f1a480dab6501320fb026297c
Steven A. Falco <stevenfalco@gmail.com>
2021-03-12 16:01:25 -0500

    Interchange blue and red for better layout

--- terminal.vhd
+++ terminal.vhd
@@ -8,7 +8,7 @@ entity terminal is
 		CLK12M			: in std_logic;				-- M2
 
 		-- DIP switches for baud rate
-		DIP_SW			: in std_logic_vector(3 downto 0);	-- N2, N1, P2, J1
+		DIP_SW			: in std_logic_vector(3 downto 0);	-- J1, P2, N1, N2
 
 		-- The FPGA can drive 8 mA per pin.  The RGB pins drive a 75 ohm
 		-- load, and we need to get it to 0.7 volts for "white".
@@ -18,12 +18,12 @@ entity terminal is
 		-- separate series resistor.  We could even get some greyscale
 		-- output if desired, by wiring an R-2R ladder.  I've seen people
 		-- use 3 or 4 independent bits per color, but we don't need that.
-		PIXEL_R1		: out std_logic;			-- R12
-		PIXEL_R2		: out std_logic;			-- T13
+		PIXEL_R1		: out std_logic;			-- P14
+		PIXEL_R2		: out std_logic;			-- R14
 		PIXEL_G1		: out std_logic;			-- R13
 		PIXEL_G2		: out std_logic;			-- T14
-		PIXEL_B1		: out std_logic;			-- P14
-		PIXEL_B2		: out std_logic;			-- R14
+		PIXEL_B1		: out std_logic;			-- R12
+		PIXEL_B2		: out std_logic;			-- T13
 
 		HSYNC			: out std_logic;			-- T15
 		VSYNC			: out std_logic;			-- N16
