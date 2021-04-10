2628e73cec6480a8eff1dc06cac6689627040d87
Steven A. Falco <stevenfalco@gmail.com>
2021-04-10 14:51:55 -0400

    Fixed stack pointer

--- fw/test.s
+++ fw/test.s
@@ -2,8 +2,13 @@
 	.align	2
 	.globl	_start
 
+	| For some reason, if I try to set a non-zero stack pointer
+	| in the reset vector, the code won't run.  I don't know why,
+	| but it is simple enough to instead set the stack pointer in
+	| the first instruction.
+
 	. = 0x000	| Reset vector
-	dc.l	0x8000	| Initial Stack Pointer (%a7)
+	dc.l	0	| Initial Stack Pointer
 	dc.l	_start	| Initial Program Counter
 	
 	. = 0x060	| Start of interrupt vectors
@@ -19,36 +24,49 @@
 	.section .text
 	.align	2
 
-_start:			| first instruction of program
+| Register usage:
+|
+| %a6 = fp
+| %a7 = sp
 
-			| %a7 = sp
-			| %a6 = fp
+_start:
+	| Set the real stack pointer to the top of RAM.  The stack
+	| pointer pre-decrements so we actually want the LWA+1 here.
+	mov.l	#0x00008000, %sp
 
-initData:
+	| Initialize the C global variables that start off as non-zero.
+	|
+	| Our linker control file specifies 4-byte alignment of the
+	| _sdata and _edata variables, so we know the number of bytes
+	| to copy is a multiple of 4.
 	mov.l	#_etext, %a0		| Initial values for data section
 	mov.l	#_sdata, %a1		| Start of data section
 	mov.l	#_edata, %d0		| End of data section
 	sub.l	%a1, %d0		| How many bytes to copy
-	beq	initBss			| Nothing to do
+	beq	initDataDone		| Nothing to do
 	lsr.l	#2, %d0			| Divide by 4
 	sub.l	#1, %d0			| Compensate dbf
 initDataLoop:
 	mov.l	(%a0)+, (%a1)+		| Copy one lword
 	dbf	%d0, initDataLoop	| Do them all
+initDataDone:
 
-initBss:
+	| Initialize the C global variables that start off as zero.
+	|
+	| Our linker control file specifies 4-byte alignment of the
+	| _sbss and _ebss variables, so we know the number of bytes
+	| to copy is a multiple of 4.
 	mov.l	#_sbss, %a0		| Start of BSS
 	mov.l	#_ebss, %d0		| End of BSS
 	sub.l	%a0, %d0		| How many bytes to zero
-	beq	startMain		| Nothing to do
+	beq	initBssDone		| Nothing to do
 	lsr.l	#2, %d0			| Divide by 4
 	sub.l	#1, %d0			| Compensate dbf
 	mov.l	#0, %d1			| Get a zero
 initBssLoop:
 	mov.l	%d1, (%a0)+		| Zero one lword
 	dbf	%d0, initBssLoop	| Do them all
-
-startMain:
+initBssDone:
 
 	mov.b	#1, %d6
 	mov.b	%d6, 0xc060		| Enable video sync
